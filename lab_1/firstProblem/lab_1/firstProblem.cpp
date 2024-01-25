dbdc141be91e3b19027be1585dd5c19f48fc217e
David Gaziev <davidgaziev6@gmail.com>
2024-01-25 19:11:46 +0600

    first lab

---
+++ lab_1/firstProblem/lab_1/firstProblem.cpp
@@ -0,0 +1,85 @@
+﻿#include <iostream>
+using namespace std;
+
+
+static double power(double n, int p = 2) {
+    double result = n;
+
+    for (int i = 1; i < p; i++) {
+        result *= n;
+    }
+
+    return result;
+}
+
+static double power(char n, int p = 2) {
+    double temp = n - '0';
+
+    double result = temp;
+
+    for (int i = 1; i < p; i++) {
+        result *= temp;
+    }
+
+    return result;
+}
+
+static double power(short int n, int p = 2) {
+    double result = n;
+
+    for (int i = 1; i < p; i++) {
+        result *= n;
+    }
+
+    return result;
+}
+
+static double power(long int n, int p = 2) {
+    double result = n;
+
+    for (int i = 1; i < p; i++) {
+        result *= n;
+    }
+
+    return result;
+}
+
+static double power(float n, int p = 2) {
+    double result = n;
+
+    for (int i = 1; i < p; i++) {
+        result *= n;
+    }
+
+    return result;
+}
+
+
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    float digit;
+    int powerDigit, answer;
+    double result;
+
+    cout << "Введите число: ";
+    cin >> digit;
+    cout << "Хотите ли ввести степень? (>=1 - да, 0 - нет): ";
+    cin >> answer;
+    if (answer) {
+        while (true) {
+            cout << "Введите степень: ";
+            cin >> powerDigit;
+            if (powerDigit > 2) break;
+            else cout << "Степень должна быть больше 2!\n";
+        }
+        result = power(digit, powerDigit);
+    }
+    else {
+        result = power(digit);
+    }
+    
+    cout << "Результат: " << result;
+}
---
+++ lab_1/secondProblem/secondProblem/secondProblem.cpp
@@ -0,0 +1,95 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <windows.h>
+
+using namespace std;
+
+class Worker {
+private:
+    string surname;
+    int age;
+    string specialty;
+    double averageSalary;
+
+public:
+    
+    Worker(const string& s, int a, const string& sp, double sal) : surname(s), age(a), specialty(sp), averageSalary(sal) {}
+
+    string getSurname() const { return surname; }
+    int getAge() const { return age; }
+    string getSpecialty() const { return specialty; }
+    double getAverageSalary() const { return averageSalary; }
+};
+
+
+class FactoryManager {
+private:
+    vector<vector<Worker>> factories; 
+
+public:
+    void addWorkers(int factoryIndex) {
+        int numWorkers;
+        cout << "Введите количество работников на заводе " << factoryIndex + 1 << ": ";
+        cin >> numWorkers;
+
+        vector<Worker> factoryWorkers; 
+
+        for (int j = 0; j < numWorkers; ++j) {
+            string surname, specialty;
+            int age;
+            double averageSalary;
+
+            cout << "Введите фамилию работника " << j + 1 << ": ";
+            cin >> surname;
+            cout << "Введите возраст работника " << j + 1 << ": ";
+            cin >> age;
+            cout << "Введите специальность работника " << j + 1 << " (слесарь/токарь): ";
+            cin >> specialty;
+            cout << "Введите средний оклад работника " << j + 1 << ": ";
+            cin >> averageSalary;
+
+            factoryWorkers.emplace_back(surname, age, specialty, averageSalary);
+        }
+
+        factories.push_back(factoryWorkers); 
+    }
+
+    void countWorkers(int& numFitters, int& numTurners) {
+        for (const auto& factory : factories) {
+            for (const auto& worker : factory) {
+                if (worker.getSpecialty() == "слесарь") {
+                    numFitters++;
+                }
+                else if (worker.getSpecialty() == "токарь") {
+                    numTurners++;
+                }
+            }
+        }
+    }
+};
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251); 
+
+    int numFactories;
+    cout << "Введите количество заводов: ";
+    cin >> numFactories;
+
+    FactoryManager factoryManager;
+
+    for (int i = 0; i < numFactories; ++i) {
+        factoryManager.addWorkers(i);
+    }
+
+    int numFitters = 0;
+    int numTurners = 0;
+    factoryManager.countWorkers(numFitters, numTurners);
+
+    cout << "Количество слесарей: " << numFitters << endl;
+    cout << "Количество токарей: " << numTurners << endl;
+
+    return 0;
+}

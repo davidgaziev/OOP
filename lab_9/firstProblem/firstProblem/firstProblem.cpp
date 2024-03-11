6b797eb3b778b39d3465bff1bfb2dfadfbcf31e7
davidgaziev <davidgaziev6@gmail.com>
2024-03-11 15:32:13 +0600

    add lab_9

---
+++ lab_9/firstProblem/firstProblem/firstProblem.cpp
@@ -0,0 +1,60 @@
+﻿// firstProblem.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <process.h>
+
+using namespace std;
+
+const int LIMIT = 5;
+
+class SafeArray {
+private:
+    int arr[LIMIT];
+public:
+    class Range {
+    public:
+        string msg; 
+        Range(string msg) : msg(msg){ }
+    };
+
+    void push(int n, int value) {
+        if (n < 0 || n >= LIMIT) {
+            throw Range("\nОшибочный индекс!\n");
+        }
+
+        arr[n] = value;
+    }
+
+    int get(int n) const{
+        if (n < 0 || n >= LIMIT) {
+            throw Range("\nОшибочный индекс!\n");
+        }
+
+        return arr[n];
+    }
+};
+
+int main() {
+    setlocale(LC_ALL, "ru");
+
+    SafeArray arr;
+
+
+    for (int i = 0; i < LIMIT; i++) {
+        arr.push(i, i * 10);
+    }
+
+    for (int i = 0; i < LIMIT; i++) {
+        cout << "Элемент #" << i << " = " << arr.get(i) << endl;
+    }
+
+    try {
+        arr.push(11, 5);
+    }
+    catch (SafeArray::Range error) {
+        cout << error.msg;
+    }
+
+    return 0;
+}
---
+++ lab_9/secondProblme/secondProblme/secondProblem.cpp
@@ -0,0 +1,125 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class SpeedExc {
+public:
+    string msg;
+    SpeedExc(string msg) : msg(msg) { }
+};
+
+class Tatra;
+
+class Kamaz {
+private:
+    string name;
+    double speed;
+
+public:
+    Kamaz(const string& name = "", double speed = 10) : name(name), speed(speed) {
+        if (speed <= 0) {
+            throw SpeedExc("\nСкорость не может быть меньше или равна 0\n");
+        }
+
+        speed = speed;
+    }
+
+    void changeSpeed(double newSpeed) {
+        if (newSpeed <= 0) {
+            throw SpeedExc("\nСкорость не может быть меньше или равна 0\n");
+        };
+        speed = newSpeed;
+    }
+
+    friend class Tatra;
+    friend int compareSpeed(Kamaz, Tatra);
+};
+
+class Tatra {
+private:
+    string name;
+    double speed;
+
+public:
+
+    Tatra(const string& name = "", double speed = 10) : name(name), speed(speed) {
+        if (speed <= 0) {
+            throw SpeedExc("\nСкорость не может быть меньше или равна 0\n");
+        }
+
+        speed = speed;
+    }
+
+    void showParam(Kamaz kamaz) const {
+        cout << "Название: " << name << endl;
+        cout << "Скорость: " << speed << " км/ч" << endl;
+
+        cout << "Название: " << kamaz.name << endl;
+        cout << "Скорость: " << kamaz.speed << " км/ч" << endl;
+    }
+
+    void changeSpeed(double newSpeed) {
+        if (newSpeed <= 0) {
+            throw SpeedExc("\nСкорость не может быть меньше или равна 0\n");
+        };
+        speed = newSpeed;
+    }
+
+    friend int compareSpeed(Kamaz, Tatra);
+};
+
+int compareSpeed(Kamaz kamaz, Tatra tatra) {
+    if (kamaz.speed > tatra.speed) return 1;
+    else if (kamaz.speed < tatra.speed) return -1;
+
+    return 0;
+}
+
+void printResult(int result) {
+    switch (result) {
+    case 1:
+        cout << "\nКамаз движется быстрее!\n\n";
+        break;
+    case -1:
+        cout << "\nТатра движется быстрее!\n\n";
+        break;
+    default:
+        cout << "\nГрузовики движутся одинаково!\n\n";
+        break;
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+
+    try {
+        Kamaz kamaz = Kamaz("kamaz", 45);
+        Tatra tatra = Tatra("tatra", 60);
+        cout << "Трасса 'Равнина': \n";
+        tatra.showParam(kamaz);
+
+        printResult(compareSpeed(kamaz, tatra));
+
+        kamaz.changeSpeed(00);
+        tatra.changeSpeed(80);
+
+        cout << "Трасса 'Горы': \n";
+        tatra.showParam(kamaz);
+
+        printResult(compareSpeed(kamaz, tatra));
+
+        kamaz.changeSpeed(50);
+        tatra.changeSpeed(50);
+
+        cout << "Трасса 'Пустыня': \n";
+        tatra.showParam(kamaz);
+
+        printResult(compareSpeed(kamaz, tatra));
+    }
+    catch (SpeedExc error) {
+        cout << error.msg;
+    }
+
+    return 0;
+}
